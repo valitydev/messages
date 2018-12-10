b2355ac85842c1fbc2509c3d2d49d9e6eeef15b5
n.pospolita <n.pospolita@rbkmoney.com>
2018-12-10 17:34:37 +0300

    Initial skeleton

---
+++ src/main/java/com/rbkmoney/messages/MessagesApplication.java
@@ -0,0 +1,12 @@
+package com.rbkmoney.messages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagesApplication extends SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagesApplication.class);
+    }
+}
